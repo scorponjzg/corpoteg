2e962efe8c9cdfefcd06cf08360f267beb32715b
Abraham Zermeño <scorpionjzg@gmail.com>
2019-06-11 18:09:24 -0500

    Validando horas trabajadas

--- js/asistencia.js
+++ js/asistencia.js
@@ -37,6 +37,37 @@ function crearCSV(tabla,nombreCSV){
 	
 }
 
+function restarHoras(inicio, fin) {
+  
+  var inicioHoras = parseInt(inicio.substr(0,2));
+  var inicioMinutos = parseInt(inicio.substr(3,2));
+  
+  var finHoras = parseInt(fin.substr(0,2));
+  var finMinutos = parseInt(fin.substr(3,2));
+
+  var transcurridoHoras = finHoras - inicioHoras;
+  var transcurridoMinutos = finMinutos - inicioMinutos;
+  
+  if (transcurridoMinutos < 0) {
+    transcurridoHoras--;
+    transcurridoMinutos = 60 + transcurridoMinutos;
+  }
+  
+  horas = transcurridoHoras.toString();
+  minutos = transcurridoMinutos.toString();
+  
+  if (horas.length < 2) {
+    horas = "0"+horas;
+  }
+  
+  if (horas.length < 2) {
+    horas = "0"+horas;
+  }
+  
+  return horas+":"+minutos;
+
+}
+
 function valida_formulario(){
 	
 	var inicial = $("#f_inicial").val();
@@ -88,7 +119,6 @@ function obtener_asistencia(){
 		var final = $("#f_final").val();
 		var servicio = $("#servicio").val();
 		var foto =  "";
-		var asistencia = "";
 		var contador_usuarios = 0;
 		var registro;
 		var listaHorario = 0;
@@ -105,12 +135,25 @@ function obtener_asistencia(){
 			var turno = "";
 			var temporal = "";
 			var asistio = 0;
+			var entrada = "";
+			var salida = "";
+			var e_comida = "";
+			var s_comida = "";
+			var t_comida = "";
+			var t_tabajado = "";
+			var primer_entrada = "";
+			var ultima_salida = "";
 			console.log(data);	
 			data.fecha.forEach(function(entry){
 			
 				data.requerimiento.forEach(function(turno){
 					
 					entry.usuarios.forEach(function(usuario){
+
+						primer_entrada = "";
+						ultima_salida = "";
+						salida = "";
+						entrada = "";
 
 						cambio_fecha = entry.fecha;
 						temporal += '<tr>'+
@@ -119,7 +162,7 @@ function obtener_asistencia(){
 								'<td>'+usuario.codigo+'</td>'+
 								'<td>'+usuario.nombre+'</td>';
 						usuario.registros.forEach(function(acceso){
-
+							
 							if(acceso.accion == 'E'){
 
 								if(hora_a_enteros(acceso.registro) >= hora_a_enteros(turno.entrada)-1 && hora_a_enteros(acceso.registro) <= hora_a_enteros(turno.te)){
@@ -129,24 +172,41 @@ function obtener_asistencia(){
 									registro = "red";
 								}
 
+								if(entrada == ""){
+
+									primer_entrada = acceso.registro;
+									entrada += '<span style="color:'+registro+'">'+acceso.accion+'-'+acceso.registro+'</span><br>';
+								}
+							
 							}else{
 									if(hora_a_enteros(acceso.registro) >= hora_a_enteros(turno.ts)-1 && hora_a_enteros(acceso.registro) <= hora_a_enteros(turno.salida)+1 ){
 										//asistio = 1;
-									registro = "green";
-								}else{
-									registro = "red";
-								}
+										registro = "green";
+									}else{
+										registro = "red";
+									}
+									
+									if(ultima_salida == "" | hora_a_enteros(acceso.registro) > hora_a_enteros(ultima_salida)){ 
+											console.log(acceso.registro);
+	 
+										salida = '<span style="color:'+registro+'">'+acceso.accion+'-'+acceso.registro+'</span><br>';
+										ultima_salida = acceso.registro;
 
-
+									} 
+								
 							}
 							
 							//console.log(hora_a_enteros(acceso.registro));
 							foto += '<img src="php/fotosAsistencia/'+acceso.foto+'" alt="Foto" class="img-rounded" style="width:60px">';
-							asistencia += '<span style="color:'+registro+'">'+acceso.accion+'-'+acceso.registro+'</span><br>';
-
+							
 						});
 						//'<td>'+ foto +'</td>'+
-							temporal += '<td>'+asistencia+'</td>'+
+							temporal += '<td>'+entrada+'</td>'+
+										'<td>N/A</td>'+
+										'<td>N/A</td>'+
+										'<td>'+salida+'</td>'+
+										'<td>N/A</td>'+
+										'<td>'+restarHoras(primer_entrada,ultima_salida)+'</td>'+
 								'</tr>';
 							foto="";
 							asistencia = "";
@@ -163,12 +223,22 @@ function obtener_asistencia(){
 						'<td></td>'+
 						'<td></td>'+
 						'<td style="color: blue">Personal solicitado: '+turno.personal+'. Asistentes: '+contador_usuarios+'.   Asitencia: '+parseInt(contador_usuarios * 100 /turno.personal) +'% </td>'+
+						'<td></td>'+
+						'<td></td>'+
+						'<td></td>'+
+						'<td></td>'+
+						'<td></td>'+
 						'<td></td></tr>';
 					}
 					contador_usuarios = 0;
 				});
 				
 				resultado += '<tr>'+
+					'<td></td>'+
+					'<td></td>'+
+					'<td></td>'+
+					'<td></td>'+
+					'<td></td>'+
 					'<td></td>'+
 					'<td></td>'+
 					'<td></td>'+
